595a82191894155fb2b9fed56be7bb5fb195fcc3
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-11-14 17:34:55 -0600

    se modifico el READ

---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  constructor(){
+    super();
+      this.state = {
+        todo:''
+      };
+    }
+
+    updateTodo(evt){
+      this.setState ({
+        todo: evt.target.value
+      });
+    }
+    addTodo(evt){
+      evt.preventDefault();
+      console.log('este es todo ');
+      console.log(this.state.todo);
+      //this.props.addTodo(this.state.todo);
+
+    }
+  
+  render() {
+    const {todo} = this.state;
+    return (
+      <div className="container m-5">
+        <form onSubmit={ e => this.addTodo(e)}>
+          <div className="row">
+            <div className="col">
+              <input type="text" className="form-control" 
+                  placeholder="Todo" id='todo'
+                  value={todo} onChange={ this.updateTodo.bind(this)}/>
+            </div>
+            <div className="col">
+              <button type="submit" className="btn btn-primary">Guardar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form
